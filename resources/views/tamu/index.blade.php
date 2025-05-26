9cdc2949d31c59fdfe12e0b8bb4b5c253673c65f
anastasyabp0302 <anastasyabp0302@gmail.com>
2025-05-26 14:46:01 +0700

    ubah fitur telepon di halaman tamu

--- resources/views/tamu/index.blade.php
+++ resources/views/tamu/index.blade.php
@@ -160,7 +160,7 @@
             </div>
             <div class="col-md-6">
                 <label for="telepon" class="form-label">No. Telepon</label>
-                <input type="text" name="telepon" class="form-control" required>
+                <input type="tel" name="telepon" class="form-control" pattern="[0-9]*" input-mode="numeric" required>
             </div>
             <div class="col-md-12">
                 <label for="alamat" class="form-label">Alamat</label>
@@ -195,7 +195,7 @@
   </div>
 </div>
 
-{{-- Script Detail --}}
+{{-- Script Detail & Validasi --}}
 <script>
     function tampilkanDetailTamu(nama, telepon, alamat, keperluan, kategori, tanggal) {
         document.getElementById('detail-nama').innerText = nama;
@@ -205,5 +205,14 @@
         document.getElementById('detail-kategori').innerText = kategori;
         document.getElementById('detail-tanggal').innerText = tanggal;
     }
+
+    // Validasi agar input No. Telepon hanya angka
+    document.addEventListener('DOMContentLoaded', function () {
+        const teleponInput = document.querySelector('input[name="telepon"]');
+        teleponInput.addEventListener('input', function () {
+            this.value = this.value.replace(/[^0-9]/g, '');
+        });
+    });
 </script>
+
 @endsection
