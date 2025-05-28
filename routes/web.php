a032464faec64041c0c5926d6f87de436bc52bd4
anastasyabp0302 <anastasyabp0302@gmail.com>
2025-05-28 12:46:04 +0700

    welcome

--- routes/web.php
+++ routes/web.php
@@ -43,11 +43,9 @@ Route::middleware(['auth', 'role:user'])->group(function () {
     Route::get('/dashboard', [User::class, 'dashboard']);
 Route::get('/admin/users/{id}', [UserController::class, 'show'])->name('user.show');
     // Route profile form tamu
-    Route::get('/tamu.index', [UserController::class, 'indexTamu'])->name('tamu.index');
     Route::get('/profile', [UserController::class, 'formTamu'])->name('form.tamu');          // tampilkan form
     Route::post('/profile', [UserController::class, 'storeTamu'])->name('form.tamu.store');  // proses simpan data
 
-    Route::get('/tamu', [UserController::class, 'indexTamu'])->name('tamu.index');
 Route::get('/tamu/create', [UserController::class, 'createTamu'])->name('tamu.create');
 Route::post('/tamu', [UserController::class, 'storeTamu'])->name('tamu.store');
 Route::get('/tamu/{tamu}', [UserController::class, 'showTamu'])->name('tamu.show');
@@ -74,12 +72,8 @@ Route::prefix('admin')->middleware('auth')->group(function () {
     Route::put('/form-input/{id}', [AdminTamuController::class, 'update'])->name('admin.tamu.update');
     Route::delete('/form-input/{id}', [AdminTamuController::class, 'destroy'])->name('admin.tamu.destroy');
 });
-Route::post('/form-tamu', [UserController::class, 'storeTamuPublik'])->name('form.tamu.store');
 
-Route::get('/daftar-tamu', [UserController::class, 'getDaftarTamu'])->name('tamu.daftar');
-Route::get('/cek-tamu/{telepon}', [UserController::class, 'cekTamu']);
-// web.php
-Route::post('/form/tamu/select', [UserController::class, 'selectTamu'])->name('form.tamu.select');
+Route::get('/tamu', [UserController::class, 'indexTamu'])->name('tamu.index');
 
 
 
